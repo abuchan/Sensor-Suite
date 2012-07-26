535b8b15b3fa79a34ce716230838fcb6ecb4bbcf
michaelcoe <michael.coe85@gmail.com>
2012-07-26 16:36:22 -0700

    Added methods and deleted unused ones.  Added an address set method and changed xb call variables.

--- or_helpers_stream.py
+++ or_helpers_stream.py
@@ -48,14 +48,11 @@ class HelpersStream(threading.Thread):
 		self.deg2count = 0
 		self.count2deg = 0
 		self.angRate = 0
-
+		self.addr = '\x30\x02'
+		
 		self.command_queue = Queue.Queue()
 
 		self.start()
-	
-	def run(self):
-		while True:
-			pass
 
 	def setFileName(self, fileName):
 		self.fileName = fileName
@@ -67,9 +64,15 @@ class HelpersStream(threading.Thread):
 	def add_sinks(self,sinks):
 		self.dispatcher.add_sinks(sinks)
 
+	def setImuData(self, telem_index, data):
+		self.imudata[telem_index] = data
+
+	def set_address(self, addr):
+		self.addr = addr
+
 	def setMotorGainsSet(self):
 		self.motor_gains_set = True
-	
+
 	def setSteeringGainsSet(self):
 		self.steering_gains_set = True
 
@@ -87,45 +90,65 @@ class HelpersStream(threading.Thread):
 	def setFlashErase(self):
 		self.flash_erased = True
 
+	def run(self):
+		while True:
+			pass
+
+
 ########## Helper functions #################
 	def xb_send(self, DEST_ADDR, status, type, data):
 		payload = chr(status) + chr(type) + ''.join(data)
 		data = [DEST_ADDR, payload]
 		self.dispatcher.dispatch(Message('xb_send', data))
 
+	def xb_safe_exit(self):
+		print "Halting xb"
+		self.xb.halt()
+		print "Closing serial"
+		self.ser.close()
+		print "Exiting..."
+
 	def resetRobot(self, DEST_ADDR):
 		print "Resetting robot..."
-		self.xb_send(DEST_ADDR, 0, command.SOFTWARE_RESET, pack('h',1))
-
+		self.xb_send(self.addr, 0, command.SOFTWARE_RESET, pack('h',1))
 
 	def sendEcho(self, msg):
-		self.xb_send(DEST_ADDR, 0, command.ECHO, msg)
+		self.xb_send(self.addr, 0, command.ECHO, msg)
 
 	def wakeRobot(self):
 		self.awake = 0;
 		while not(self.awake):
 			print "Waking robot ... "
-			self.xb_send(DEST_ADDR, 0, command.SLEEP, pack('b',0))
+			self.xb_send(self.addr, 0, command.SLEEP, pack('b',0))
 			time.sleep(0.2)
 
 	def sleepRobot(self):
 		print "Sleeping robot ... "
-		self.xb_send(DEST_ADDR, 0, command.SLEEP, pack('b',1))
+		self.xb_send(self.addr, 0, command.SLEEP, pack('b',1))
 
-
-	def setMotorGains(self, DEST_ADDR, gains):
-		count = 1
+	def setSteeringRate(self, rate):
+		self.angRateDeg = rate
+		self.deg2count = 14.375
+		self.count2deg = 1/self.deg2count
+		self.angRate = round( self.angRateDeg / self.count2deg)
+		self.xb_send(self.addr, 0, command.SET_CTRLD_TURN_RATE, pack('h',self.angRate))
+		time.sleep(0.3)
+		
+	def setMotorGains(self, gains):
 		self.motorGains = gains
-#		while not(self.motor_gains_set):
-		print "Setting motor gains...   ",count,"/8"
-		self.xb_send(DEST_ADDR, \
+		print "Setting motor gains...   "
+		self.xb_send(self.addr, \
 						0, command.SET_PID_GAINS, pack('10h',*gains))
 		time.sleep(0.3)
-#			if count > 8:
-#				print "Unable to set motor gains, exiting."
-#				xb_safe_exit()
 
-	def setMotorSpeeds(self, DEST_ADDR, spleft, spright):
+	def setSteeringGains(self, gains):
+		self.steeringGains = gains
+		print "Setting steering gains...   "
+		self.xb_send(self.addr, \
+						0, command.SET_STEERING_GAINS, pack('6h',*gains))
+		time.sleep(0.3)
+
+	def setMotorSpeeds(self, spleft, spright):
 		thrust = [spleft, 0, spright, 0, 0]
-		self.xb_send(DEST_ADDR, \
+		self.xb_send(self.addr, \
 						0, command.SET_THRUST_CLOSED_LOOP, pack('5h',*thrust))
