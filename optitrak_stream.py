e6d9360bb20832f66ba91f1dbdd02e6ebc107ada
michaelcoe <michael.coe85@gmail.com>
2012-08-03 10:44:45 -0700

    Added support for positioning data on multiple rigid bodies

--- optitrak_stream.py
+++ optitrak_stream.py
@@ -40,12 +40,18 @@ class OptitrakStream(threading.Thread):
 
 	def add_sinks(self,sinks):
 		self.dispatcher.add_sinks(sinks)
-	
+
+	#gets data from the optitrak stream
 	def get_Net_Callback(self, dataFrame):
 		body = dataFrame.RigidBodies[0]
-#		"x %.2f  y %.2f  z %.2f  qx %.4f  qy %.4f  qz %.4f qw %.4f" % 
-		self.input_data = [body.x, body.y, body.z, body.qx, body.qy, body.qz, body.qw]
-	
+		#comment out and delete all body2 from self.input_data if only tracking 1 body
+		body2 = dataFrame.RigidBodies[1]
+#		body3 = dataFrame.RigidBodies[2]
+#		"x %.2f  y %.2f  z %.2f  qx %.4f  qy %.4f  qz %.4f qw %.4f x2 %.2f y2 %.2f z2 %.2f" %
+		self.input_data = [body.x, body.y, body.z, body.qx, body.qy, body.qz, body.qw,
+											body2.x, body2.y, body2.z]
+
+	#sends the most current data when called by virtual.py
 	def get_optitrak_position(self):
 		self.dispatcher.dispatch(Message('optitrak_data', self.input_data))
 		
